06e0d6a589e515d05db5683d32bb9057e884329c
deyizhang <deyizhang2003@gmail.com>
2020-04-16 20:31:54 -0400

    First Push

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Image.network(
+              "https://i.pinimg.com/originals/b0/92/5d/b0925d893d3b6f8b8b87a7f9b3438480.jpg"),
+        ),
+        appBar:
+            AppBar(title: Text("I am Poor"), backgroundColor: Colors.lightBlue),
+      ),
+    ),
+  );
+}
